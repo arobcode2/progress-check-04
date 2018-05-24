921cec0352b555a54f7058a43516fb33b00c43c2
Andrew Rob <andrew-c-rob@live.com>
2018-05-24 11:59:36 -0500

    Making progress on the checkpoint

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // YOUR CODE HERE
+$(document).ready(function() {
+	$('.color').click(function() {
+		var dateText = Date.now();
+		for (var i = 0; i < $('.color').length; i++) {
+			console.log($('.color')[i].style.backgroundColor);
+			if (i === 0) {
+				$('#times').css('color', $('.color')[i].style.backgroundColor);
+			}
+		}
+		$('<div>' + dateText + '</div>').appendTo('#times');
+	});
+});
