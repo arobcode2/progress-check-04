1955ebee08d3660d1941d5b50bb817edf4fe5c83
Andrew Rob <andrew-c-rob@live.com>
2018-05-24 18:08:29 -0500

    Completed progress check 4

--- app.js
+++ app.js
@@ -2,12 +2,16 @@
 $(document).ready(function() {
 	$('.color').click(function() {
 		var dateText = Date.now();
-		for (var i = 0; i < $('.color').length; i++) {
-			console.log($('.color')[i].style.backgroundColor);
-			if (i === 0) {
-				$('#times').css('color', $('.color')[i].style.backgroundColor);
-			}
+		if ($(this).attr("id") === "1") {
+			$('<div>' + dateText + '</div>').css('color', $('.color')[0].style.backgroundColor).appendTo('#times');
+		} else if ($(this).attr("id") === "2") {
+			$('<div>' + dateText + '</div>').css('color', $('.color')[1].style.backgroundColor).appendTo('#times');
+		} else if ($(this).attr("id") === "3") {
+			$('<div>' + dateText + '</div>').css('color', $('.color')[2].style.backgroundColor).appendTo('#times');
+		} else if ($(this).attr("id") === "4") {
+			$('<div>' + dateText + '</div>').css('color', $('.color')[3].style.backgroundColor).appendTo('#times');
+		} else if ($(this).attr("id") === "5") {
+			$('<div>' + dateText + '</div>').css('color', $('.color')[4].style.backgroundColor).appendTo('#times');
 		}
-		$('<div>' + dateText + '</div>').appendTo('#times');
 	});
 });
